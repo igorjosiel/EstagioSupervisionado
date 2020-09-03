1c1faf5fc418afab108564c1f1800a57bc0f69b6
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-03 14:18:58 -0300

    Aprendendo sobre checkbox e a capturar valores selecionados

--- ComponentesBasicos/app/src/main/java/com/example/componentesbasicos/MainActivity.java
+++ ComponentesBasicos/app/src/main/java/com/example/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+    private CheckBox checkVerde, checkAmarelo, checkAzul, checkPreto, checkVermelho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +26,58 @@ public class MainActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.editNome);
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textResult);
+
+        // Checkbox
+        checkAmarelo = findViewById(R.id.checkAmarelo);
+        checkVerde = findViewById(R.id.checkVerde);
+        checkAzul = findViewById(R.id.checkAzul);
+        checkPreto = findViewById(R.id.checkPreto);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+
+    public void checkbox()
+    {
+        String texto = "";
+
+        if(checkVerde.isChecked())
+        {
+            //String corSelecionada = checkVerde.getText().toString();
+            texto += "Verde selecionado - ";
+        }
+
+        if(checkPreto.isChecked())
+        {
+            texto += "Preto selecionado - ";
+        }
+
+        if(checkVermelho.isChecked())
+        {
+            texto += "Vermelho selecionado - ";
+        }
+
+        if(checkAmarelo.isChecked())
+        {
+            texto += "Amarelo selecionado - ";
+        }
+
+        if(checkAzul.isChecked())
+        {
+            texto += "Azul selecionado - ";
+        }
+
+        textoResultado.setText(texto);
     }
 
     public void enviar(View view)
     {
+        this.checkbox();
+
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
         textoResultado.setText("nome: " + nome + "E-mail: " + email);
+         */
     }
 
     public void limpar(View view)
