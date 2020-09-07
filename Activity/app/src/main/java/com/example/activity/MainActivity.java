0e244a54aaa15a933db95cc5d8aed416f53c4f1b
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-07 11:11:55 -0300

    Aprendendo um pouco sobre como passar valores de uma activity para outra para manipulação

--- Activity/app/src/main/java/com/example/activity/MainActivity.java
+++ Activity/app/src/main/java/com/example/activity/MainActivity.java
@@ -2,58 +2,33 @@ package com.example.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
-    }
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
 
-    protected void onStart()
-    {
-        super.onStart();
+                //Passar dados
+                intent.putExtra("nome", "Jamilton");
+                intent.putExtra("idade", 30);
 
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
     }
 }
