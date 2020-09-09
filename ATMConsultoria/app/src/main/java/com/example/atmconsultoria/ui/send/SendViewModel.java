c9773fe1820a1fd8ae9453f00d396ede4dd7a604
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-09 19:49:25 -0300

    Iniciando um novo app aplicando os conheciecimentos aprendidos sobre NavigationDrawer, montando a interface

--- ATMConsultoria/app/src/main/java/com/example/atmconsultoria/ui/send/SendViewModel.java
+++ ATMConsultoria/app/src/main/java/com/example/atmconsultoria/ui/send/SendViewModel.java
@@ -1,16 +1,16 @@
-package com.example.navigationdrawer.ui.tools;
+package com.example.atmconsultoria.ui.send;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ToolsViewModel extends ViewModel {
+public class SendViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public ToolsViewModel() {
+    public SendViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is tools fragment");
+        mText.setValue("This is send fragment");
     }
 
     public LiveData<String> getText() {
