c693988a2a00005b212a3820cf34fe30ae815181
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-10 16:33:45 -0300

    Finalizando o app de manipulação de mídias de áudio controlando o volume

--- MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekbarVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+
+        this.inicializarSeekbar();
+    }
+
+    private void inicializarSeekbar()
+    {
+        seekbarVolume = findViewById(R.id.seekBarVolume);
+
+        //Configurar o audio manager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //Recupera os valores de volume máximo e o volume atual
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //Configurar os valores máximos para o Seekbar
+        seekbarVolume.setMax(volumeMaximo);
+
+        //Configurar o processo atual do seekbar
+        seekbarVolume.setProgress(volumeAtual);
+
+        seekbarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void executarSom(View view)
@@ -33,5 +75,24 @@ public class MainActivity extends AppCompatActivity {
         if (mediaPlayer.isPlaying()) mediaPlayer.stop();
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null && mediaPlayer.isPlaying())
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mediaPlayer.isPlaying()) mediaPlayer.pause();
     }
 }
