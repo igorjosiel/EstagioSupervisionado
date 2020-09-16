dc55bef8543e543973c1e7207bc11e6d8778f025
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-16 08:24:29 -0300

    Configurando a estrutura do banco de dados no app

--- ListaTarefas/app/src/main/java/com/example/listatarefas/activity/AdicionarTarefaActivity.java
+++ ListaTarefas/app/src/main/java/com/example/listatarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package com.example.listatarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.listatarefas.R;
 
@@ -12,5 +16,24 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.itemSalvar :
+                Toast.makeText(AdicionarTarefaActivity.this, "Botão salvar pressionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
