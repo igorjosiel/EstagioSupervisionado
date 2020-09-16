325ebe8abba9623ff8f53ef7fe72e1dfb3e6205a
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-16 14:28:01 -0300

    Implementando a lógica de atualização dos registros no banco de dados

--- ListaTarefas/app/src/main/java/com/example/listatarefas/activity/AdicionarTarefaActivity.java
+++ ListaTarefas/app/src/main/java/com/example/listatarefas/activity/AdicionarTarefaActivity.java
@@ -16,6 +16,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class AdicionarTarefaActivity extends AppCompatActivity {
 
     private TextInputEditText editTarefa;
+    private Tarefa tarefaAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,15 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_adicionar_tarefa);
 
         editTarefa = findViewById(R.id.textTarefa);
+
+        //Recuperar tarefa, caso seja edição
+        tarefaAtual = (Tarefa) getIntent().getSerializableExtra("tarefaSelecionada");
+
+        //Configurar tarefa na caixa dde texto
+        if (tarefaAtual != null)
+        {
+            editTarefa.setText(tarefaAtual.getNomeTarefa());
+        }
     }
 
     @Override
@@ -38,17 +48,52 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
         {
             case R.id.itemSalvar :
                 //Executa ação para o item salvar
+
                 TarefaDAO tarefaDAO = new TarefaDAO(getApplicationContext());
 
-                String nomeTarefa = editTarefa.getText().toString();
-
-                if (!nomeTarefa.isEmpty())
+                if (tarefaAtual != null)
                 {
-                    Tarefa tarefa = new Tarefa();
-                    tarefa.setNomeTarefa(nomeTarefa);
+                    String nomeTarefa = editTarefa.getText().toString();
 
-                    tarefaDAO.salvar(tarefa);
-                    finish();
+                    if (!nomeTarefa.isEmpty())
+                    {
+                        Tarefa tarefa = new Tarefa();
+                        tarefa.setNomeTarefa(nomeTarefa);
+                        tarefa.setId(tarefaAtual.getId());
+
+                        //Atualizar no banco de dados
+                        if (tarefaDAO.atualizar(tarefa))
+                        {
+                            finish();
+
+                            Toast.makeText(getApplicationContext(), "Sucesso ao atualizar tarefa!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Erro ao atualizar tarefa!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else
+                {
+                    String nomeTarefa = editTarefa.getText().toString();
+
+                    if (!nomeTarefa.isEmpty())
+                    {
+                        Tarefa tarefa = new Tarefa();
+                        tarefa.setNomeTarefa(nomeTarefa);
+
+                        if (tarefaDAO.salvar(tarefa))
+                        {
+                            finish();
+
+                            Toast.makeText(getApplicationContext(), "Sucesso ao salvar tarefa!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Erro ao salvar tarefa!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
                 }
 
                 break;
--- ListaTarefas/app/src/main/java/com/example/listatarefas/activity/MainActivity.java
+++ ListaTarefas/app/src/main/java/com/example/listatarefas/activity/MainActivity.java
@@ -58,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
                         new RecyclerItemClickListener.OnItemClickListener() {
                             @Override
                             public void onItemClick(View view, int position) {
+                                //Recuperar tarefa para edição
+                                Tarefa tarefaSelecionada = listaTarefas.get(position);
 
+                                //Envia tarefa para tela adicionar tarefa
+                                Intent intent = new Intent(MainActivity.this, AdicionarTarefaActivity.class);
+                                intent.putExtra("tarefaSelecionada", tarefaSelecionada);
+
+                                startActivity(intent);
                             }
 
                             @Override
