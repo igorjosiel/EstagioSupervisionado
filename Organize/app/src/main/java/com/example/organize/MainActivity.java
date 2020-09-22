a18c8e101b8a23d39c0ccc7da56a4a1c141f3d10
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-22 08:25:16 -0300

    Configurando as interfaces de cadastro e loguin de usuário

--- Organize/app/src/main/java/com/example/organize/MainActivity.java
+++ Organize/app/src/main/java/com/example/organize/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.organize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.organize.activity.CadastroActivity;
+import com.example.organize.activity.LoguinActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -40,5 +44,21 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.intro_4)
                 .build()
         );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build()
+        );
+    }
+
+    public void btEntrar(View view)
+    {
+        startActivity(new Intent(this, LoguinActivity.class));
+    }
+
+    public void btCadastrar(View view)
+    {
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
