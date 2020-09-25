e08d454f634ec96dcf142c1372f876a4cd1ced10
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-24 22:27:27 -0300

    Aprendendo um pouco sobre utilização de calendários em apps com o uso de uma biblioteca para isso

---
+++ Calendario/app/src/main/java/com/example/jamiltondamasceno/calendario/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.jamiltondamasceno.calendario;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+
+        /*calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2015,1,1))
+                .setMaximumDate(CalendarDay.from(2018,3,1))
+                .commit();*/
+        CharSequence meses[] = {"Janeiro","Fevereiro", "Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths( meses );
+
+        CharSequence semanas[] = {"Segunda", "Terça","Quarta","Quinta","Sexta","Sábado","Domingo"};
+        calendarView.setWeekDayLabels(semanas);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data: ", "valor: " + (date.getMonth() + 1) + "/" + date.getYear() );
+            }
+        });
+
+        /*
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Log.i("data: ", "valor: " + date );
+            }
+        });*/
+
+        /*
+        calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.i("data: ", "valor: " + dayOfMonth + "/" + month + "/" + year );
+            }
+        });*/
+
+    }
+}
