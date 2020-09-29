d9aedf18a632d746517f06a964d362f4cde75dd5
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-09-29 09:01:23 -0300

    Criando layout de cadastro

--- WatsAppClone/app/src/main/java/com/example/watsappclone/LoguinActivity.java
+++ WatsAppClone/app/src/main/java/com/example/watsappclone/LoguinActivity.java
@@ -2,7 +2,9 @@ package com.example.watsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoguinActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoguinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loguin);
+    }
+
+    public void abrirTelaCadastro(View view)
+    {
+        Intent intent = new Intent(LoguinActivity.this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
