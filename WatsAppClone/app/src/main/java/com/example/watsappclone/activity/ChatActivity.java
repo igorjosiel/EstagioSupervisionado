99fef226467f2d24e4a5c5752bd69f421d63aa92
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-10-06 09:54:11 -0300

    Configurando os layouts de chat para destinatário e remetente

--- WatsAppClone/app/src/main/java/com/example/watsappclone/activity/ChatActivity.java
+++ WatsAppClone/app/src/main/java/com/example/watsappclone/activity/ChatActivity.java
@@ -4,17 +4,30 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.example.watsappclone.adapter.MensagensAdapter;
+import com.example.watsappclone.config.ConfiguracaoFirebase;
+import com.example.watsappclone.helper.Base64Custom;
+import com.example.watsappclone.helper.UsuarioFirebase;
+import com.example.watsappclone.model.Mensagem;
 import com.example.watsappclone.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.watsappclone.R;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,7 +35,15 @@ public class ChatActivity extends AppCompatActivity {
 
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
+    private EditText editMensagem;
     private Usuario usuarioDestinatario;
+
+    //Identificador usuários: remetente e destinatário
+    private String idUsuarioRemetente, idUsuarioDestinatario;
+
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter adapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +60,11 @@ public class ChatActivity extends AppCompatActivity {
         //Configurações iniciais
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFoto);
+        editMensagem = findViewById(R.id.editMensagem);
+        recyclerMensagens = findViewById(R.id.recyclerMensagens);
+
+        //Recuperar os dados do usuário remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //Recuperar dados do usuário destinatário
         Bundle bundle = getIntent().getExtras();
@@ -58,6 +84,51 @@ public class ChatActivity extends AppCompatActivity {
             {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
+
+            //Recuperar dados do usuário destinatário
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
+
+        //Configurar adapter
+        adapter = new MensagensAdapter(mensagens, getApplicationContext());
+
+        //Configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(adapter);
+    }
+
+    public void enviarMensagem(View view)
+    {
+        String textoMensagem = editMensagem.getText().toString();
+
+        if (!textoMensagem.isEmpty())
+        {
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            //Salvar mensagem
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+        } else
+        {
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem mensagem)
+    {
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(mensagem);
+
+        //Limpar texto
+        editMensagem.setText("");
     }
 }
---
+++ WatsAppClone/app/src/main/java/com/example/watsappclone/model/Mensagem.java
@@ -0,0 +1,36 @@
+package com.example.watsappclone.model;
+
+public class Mensagem {
+
+    private String idUsuario;
+
+    public Mensagem() {
+    }
+
+    private String mensagem;
+    private String imagem;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
