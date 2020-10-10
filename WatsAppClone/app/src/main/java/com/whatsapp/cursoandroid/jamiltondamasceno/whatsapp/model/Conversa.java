592d94418bfa419972d6f3becbc5d038634bfeb9
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-10-10 10:37:26 -0300

    Enviando os contatos para cadastro no grupo

---
+++ WatsAppClone/app/src/main/java/com/whatsapp/cursoandroid/jamiltondamasceno/whatsapp/model/Conversa.java
@@ -0,0 +1,63 @@
+package com.whatsapp.cursoandroid.jamiltondamasceno.whatsapp.model;
+
+import com.google.firebase.database.DatabaseReference;
+import com.whatsapp.cursoandroid.jamiltondamasceno.whatsapp.config.ConfiguracaoFirebase;
+
+/**
+ * Created by jamiltondamasceno
+ */
+
+public class Conversa {
+
+    private String idRemetente;
+    private String idDestinatario;
+    private String ultimaMensagem;
+    private Usuario usuarioExibicao;
+
+    public Conversa() {
+    }
+
+    public void salvar(){
+
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference conversaRef = database.child("conversas");
+
+        conversaRef.child( this.getIdRemetente() )
+                .child( this.getIdDestinatario() )
+                .setValue( this );
+
+    }
+
+    public Usuario getUsuarioExibicao() {
+        return usuarioExibicao;
+    }
+
+    public void setUsuarioExibicao(Usuario usuarioExibicao) {
+        this.usuarioExibicao = usuarioExibicao;
+    }
+
+    public String getIdRemetente() {
+        return idRemetente;
+    }
+
+    public void setIdRemetente(String idRemetente) {
+        this.idRemetente = idRemetente;
+    }
+
+    public String getIdDestinatario() {
+        return idDestinatario;
+    }
+
+    public void setIdDestinatario(String idDestinatario) {
+        this.idDestinatario = idDestinatario;
+    }
+
+    public String getUltimaMensagem() {
+        return ultimaMensagem;
+    }
+
+    public void setUltimaMensagem(String ultimaMensagem) {
+        this.ultimaMensagem = ultimaMensagem;
+    }
+
+}
