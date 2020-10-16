0d6de7eb4fddbe9cc4c95a7e3ae2ed56cd0bcefa
vanessa keli <igorjosielvitalino@gmial.com>
2020-10-16 18:55:13 -0300

    Criando um Toobar com um item de sair para deslogar o usuário e voltar para a tela de loguin

--- Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/activity/MainActivity.java
+++ Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/activity/MainActivity.java
@@ -1,15 +1,64 @@
 package com.instagram.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.instagram.R;
+import com.instagram.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar( toolbar );
+
+        //configuracoes de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_sair :
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
