318f8e257bb099fb8485cf7b52ac73ffdc1c2357
vanessa keli <igorjosielvitalino@gmial.com>
2020-10-17 13:40:19 -0300

    Desenvolvendo tela de pesquisa e usando o SearchView para fazer certos filtros

--- Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/fragment/PesquisaFragment.java
+++ Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/fragment/PesquisaFragment.java
@@ -2,6 +2,9 @@ package com.instagram.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,9 @@ import android.view.ViewGroup;
 import com.instagram.R;
 
 public class PesquisaFragment extends Fragment {
+
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerPesquisa;
 
     public PesquisaFragment() {
         
@@ -18,6 +24,27 @@ public class PesquisaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         
-        return inflater.inflate(R.layout.fragment_pesquisa, container, false);
+        View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
+        searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+        recyclerPesquisa   = view.findViewById(R.id.recyclerPesquisa);
+
+        //Configuração do searchview para pesquisa
+        searchViewPesquisa.setQueryHint("Buscar usuários");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return true;
+            }
+        });
+
+        return view;
     }
 }
