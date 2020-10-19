32e7187257e3cdf7981066859931389242da7d6f
vanessa keli <igorjosielvitalino@gmial.com>
2020-10-18 22:49:08 -0300

    Desenvolvendo interface de filtros para a imagem

--- Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/activity/FiltroActivity.java
+++ Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/activity/FiltroActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.instagram.R;
@@ -20,6 +24,13 @@ public class FiltroActivity extends AppCompatActivity {
 
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
 
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar( toolbar );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
         //Recupera a imagem escolhida pelo usuário
         Bundle bundle = getIntent().getExtras();
         if( bundle != null ){
@@ -27,5 +38,32 @@ public class FiltroActivity extends AppCompatActivity {
             imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length );
             imageFotoEscolhida.setImageBitmap( imagem );
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.ic_salvar_postagem :
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
