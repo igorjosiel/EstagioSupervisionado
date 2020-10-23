f5b3c8042d090994a18e7b16494b1a92d9257897
vanessa keli <igorjosielvitalino@gmial.com>
2020-10-23 19:30:12 -0300

    Recuperando feed de postagens do usuário com possibilidade de curtir e comentar

---
+++ Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/adapter/AdapterFeed.java
@@ -0,0 +1,77 @@
+package com.instagram.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.instagram.R;
+import com.instagram.model.Feed;
+import com.like.LikeButton;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AdapterFeed extends RecyclerView.Adapter<AdapterFeed.MyViewHolder> {
+
+    private List<Feed> listaFeed;
+    private Context context;
+
+    public AdapterFeed(List<Feed> listaFeed, Context context) {
+        this.listaFeed = listaFeed;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_feed, parent, false);
+        return new AdapterFeed.MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Feed feed = listaFeed.get(position);
+
+        //Carrega dados do feed
+        Uri uriFotoUsuario = Uri.parse( feed.getFotoUsuario() );
+        Uri uriFotoPostagem = Uri.parse( feed.getFotoPostagem() );
+
+        Glide.with( context ).load( uriFotoUsuario ).into(holder.fotoPerfil);
+        Glide.with( context ).load( uriFotoPostagem ).into(holder.fotoPostagem);
+
+        holder.descricao.setText( feed.getDescricao() );
+        holder.nome.setText( feed.getNomeUsuario() );
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaFeed.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView fotoPerfil;
+        TextView nome, descricao, qtdCurtidas;
+        ImageView fotoPostagem, visualizarComentario;
+        LikeButton likeButton;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            fotoPerfil   = itemView.findViewById(R.id.imagePerfilPostagem);
+            fotoPostagem = itemView.findViewById(R.id.imagePostagemSelecionada);
+            nome         = itemView.findViewById(R.id.textPerfilPostagem);
+            qtdCurtidas  = itemView.findViewById(R.id.textQtdCurtidasPostagem);
+            descricao    = itemView.findViewById(R.id.textDescricaoPostagem);
+            visualizarComentario    = itemView.findViewById(R.id.imageComentarioFeed);
+            likeButton = itemView.findViewById(R.id.likeButtonFeed);
+        }
+    }
+}
--- Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/fragment/FeedFragment.java
+++ Instagram/app/src/main/java/com/instagram/cursoandroid/igorjosiel/instagram/fragment/FeedFragment.java
@@ -1,23 +1,90 @@
 package com.instagram.fragment;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.instagram.R;
+import com.instagram.adapter.AdapterFeed;
+import com.instagram.helper.ConfiguracaoFirebase;
+import com.instagram.helper.UsuarioFirebase;
+import com.instagram.model.Feed;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FeedFragment extends Fragment {
 
+    private RecyclerView recyclerFeed;
+    private AdapterFeed adapterFeed;
+    private List<Feed> listaFeed = new ArrayList<>();
+    private ValueEventListener valueEventListenerFeed;
+    private DatabaseReference feedRef;
+    private String idUsuarioLogado;
+
     public FeedFragment() {
-        }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        //Configurações iniciais
+        idUsuarioLogado = UsuarioFirebase.getIdentificadorUsuario();
+        feedRef = ConfiguracaoFirebase.getFirebase()
+                .child("feed")
+                .child( idUsuarioLogado );
+
+        recyclerFeed = view.findViewById(R.id.recyclerFeed);
+
+        //Configuração do recyclerview
+        adapterFeed = new AdapterFeed(listaFeed, getActivity() );
+        recyclerFeed.setHasFixedSize(true);
+        recyclerFeed.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerFeed.setAdapter( adapterFeed );
+
+        return view;
+    }
+
+    private void listarFeed(){
+
+        valueEventListenerFeed = feedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for ( DataSnapshot ds: dataSnapshot.getChildren() ){
+                    listaFeed.add( ds.getValue(Feed.class) );
+                }
+                Collections.reverse( listaFeed );
+                adapterFeed.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        listarFeed();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        feedRef.removeEventListener( valueEventListenerFeed );
     }
 }
