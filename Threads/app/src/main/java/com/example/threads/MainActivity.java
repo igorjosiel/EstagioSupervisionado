e3123d585fc88ce72fac45bf3e8b3d256cc45853
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-10-31 18:34:18 -0300

    Aprendendo um pouco sobre Threads e como elas funcionam

---
+++ Threads/app/src/main/java/com/example/threads/MainActivity.java
@@ -0,0 +1,54 @@
+package com.threads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void iniciarThread(View view){
+        MyRunnable runnable = new MyRunnable();
+        new Thread( runnable ).start();
+    }
+
+    class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i=0; i <= 15; i++ ){
+
+                Log.d("Thread", "contador: " + i );
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            for (int i=0; i <= 15; i++ ){
+
+                Log.d("Thread", "contador: " + i );
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
