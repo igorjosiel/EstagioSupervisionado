63d45710d0bf2de4afcf6b7c1a35c7e0e1b0e1b9
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-11-06 20:34:06 -0200

    Implementando lógica para uma lista no spinner para exibir entre vários valores a escolher um

--- OLXApp/app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
+++ OLXApp/app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
@@ -1,32 +1,46 @@
 package com.olxapp.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.olxapp.R;
 import com.olxapp.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity
+            implements View.OnClickListener {
 
     private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
+    private Spinner campoEstado, campoCategoria;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
 
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +50,7 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializarComponentes();
+        carregarDadosSpinner();
     }
 
     public void salvarAnuncio(View view){
@@ -44,12 +59,85 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         Log.d("salvar", "salvarAnuncio: " + valor );
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.d("onClick", "onClick: " + v.getId() );
+        switch ( v.getId() ){
+            case R.id.imageCadastro1 :
+                Log.d("onClick", "onClick: " );
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2 :
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3 :
+                escolherImagem(3);
+                break;
+        }
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == Activity.RESULT_OK){
+
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configura imagem no ImageView
+            if( requestCode == 1 ){
+                imagem1.setImageURI( imagemSelecionada );
+            }else if( requestCode == 2 ){
+                imagem2.setImageURI( imagemSelecionada );
+            }else if( requestCode == 3 ){
+                imagem3.setImageURI( imagemSelecionada );
+            }
+
+            listaFotosRecuperadas.add( caminhoImagem );
+        }
+    }
+
+    private void carregarDadosSpinner(){
+
+        //Configura spinner de estados
+        String[] estados = getResources().getStringArray(R.array.estados);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                estados
+        );
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        campoEstado.setAdapter( adapter );
+
+        //Configura spinner de categorias
+        String[] categorias = getResources().getStringArray(R.array.categorias);
+        ArrayAdapter<String> adapterCategoria = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                categorias
+        );
+        adapterCategoria.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        campoCategoria.setAdapter( adapterCategoria );
+    }
+
     private void inicializarComponentes(){
 
         campoTitulo = findViewById(R.id.editTitulo);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editValor);
         campoTelefone = findViewById(R.id.editTelefone);
+        campoEstado = findViewById(R.id.spinnerEstado);
+        campoCategoria = findViewById(R.id.spinnerCategoria);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
 
         //Configura localidade para pt -> portugues BR -> Brasil
         Locale locale = new Locale("pt", "BR");
