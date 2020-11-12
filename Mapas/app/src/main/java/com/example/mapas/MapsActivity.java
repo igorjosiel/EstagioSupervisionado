40a15f02bd7e9c1a30b1ea198542b90fe1b3b250
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-11-11 23:05:36 -0200

    Desenhando formas no mapa, como polígonos, triângulos, linhas utilizando como base para isso longitude e latitude

--- Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
+++ Mapas/app/src/main/java/com/example/mapas/MapsActivity.java
@@ -1,15 +1,20 @@
-package com.mapas.sdkmapasgoogle;
+package com.sdkmapasgoogle;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -29,18 +34,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //Mudar exibição do mapa
         mMap.setMapType( GoogleMap.MAP_TYPE_NORMAL );
 
-        LatLng ibirapuera = new LatLng(-23.587097, -46.657635);
+        final LatLng ibirapuera = new LatLng(-23.587097, -46.657635);
+
+        //Adicionando evento de clique no mapa
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                PolylineOptions polylineOptions = new PolylineOptions();
+                polylineOptions.add(ibirapuera);
+                polylineOptions.add( latLng );
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.width(20);
+
+                mMap.addPolyline( polylineOptions );
+
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position( latLng )
+                                .title("Local")
+                                .snippet("Descricao")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_loja))
+                );
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                Toast.makeText(MapsActivity.this,
+                        "onLong Lat: " + latitude + " long:" + longitude ,
+                        Toast.LENGTH_SHORT).show();
+
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position( latLng )
+                                .title("Local")
+                                .snippet("Descricao")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.carro))
+                );
+            }
+        });
 
         mMap.addMarker(
                 new MarkerOptions()
                         .position( ibirapuera )
                         .title("Ibirapuera")
-                       
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_carro_roxo_48px))
         );
+
         mMap.moveCamera(//2.0 até 21.0
               CameraUpdateFactory.newLatLngZoom(ibirapuera,18)
         );
