228dc4d86b0b42103ca665b2b4c828ff99079106
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-11-14 10:38:20 -0200

    Criando interface de viagem e com recurso para colocar o local de destino, com um mapa

--- UberApp/app/src/main/java/com/example/uberapp/helper/UsuarioFirebase.java
+++ UberApp/app/src/main/java/com/example/uberapp/helper/UsuarioFirebase.java
@@ -1,5 +1,7 @@
 package com.uber.helper;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -8,7 +10,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.uber.activity.PassageiroActivity;
+import com.uber.activity.RequisicoesActivity;
 import com.uber.config.ConfiguracaoFirebase;
+import com.uber.model.Usuario;
 
 public class UsuarioFirebase {
 
@@ -40,5 +49,40 @@ public class UsuarioFirebase {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static void redirecionaUsuarioLogado(final Activity activity){
+
+        FirebaseUser user = getUsuarioAtual();
+        if(user != null ){
+            DatabaseReference usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase()
+                    .child("usuarios")
+                    .child( getIdentificadorUsuario() );
+            usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    Usuario usuario = dataSnapshot.getValue( Usuario.class );
+
+                    String tipoUsuario = usuario.getTipo();
+                    if( tipoUsuario.equals("M") ){
+                        Intent i = new Intent(activity, RequisicoesActivity.class);
+                        activity.startActivity(i);
+                    }else {
+                        Intent i = new Intent(activity, PassageiroActivity.class);
+                        activity.startActivity(i);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    public static String getIdentificadorUsuario(){
+        return getUsuarioAtual().getUid();
     }
 }
