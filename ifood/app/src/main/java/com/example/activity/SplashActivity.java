078450bd4f4d945681a687e510ca5d0eb8330465
Igor Josiel Vitalino Ribeiro <igor@IgorLinux.Igor@.com>
2020-11-21 10:02:46 -0200

    Iniciando novo app. Ifood, criando a interface inicial de acesso para logar e o processo de autenticação

---
+++ ifood/app/src/main/java/com/example/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package br.ifood.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.ifood.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abrirAutenticacao();
+            }
+        }, 3000);
+    }
+
+    private void abrirAutenticacao(){
+        Intent i = new Intent(SplashActivity.this, AutenticacaoActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
